6f904e5ceaeb1a0fbdf62f049ac7e1d7b190e382
Edrees <mohamededrees234@hotmail.com>
2024-03-01 23:21:41 +0200

    integrated firebase & crashlytics

--- app/src/main/java/com/training/ecommerce/MainActivity.kt
+++ app/src/main/java/com/training/ecommerce/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.training.ecommerce
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
